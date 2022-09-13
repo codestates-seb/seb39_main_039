eb843e0c58b3ad9890d89c9600c066a04f2ac1cf
jindory <laonjay@gmail.com>
2022-09-13 15:28:08 +0900

    chore. pretendard 폰트 적용

--- client/src/GlobalStyle.js
+++ client/src/GlobalStyle.js
@@ -1,12 +1,21 @@
 import { createGlobalStyle } from "styled-components";
+import "./assets/fonts/font.css";
 import reset from "styled-reset";
 
 const GlobalStyle = createGlobalStyle`
- ${reset}
- *{
-    box-sizing: border-box;
- }
+   ${reset}
 
+   *{
+      box-sizing: border-box;
+   }
+
+   body {
+   font-family: -apple-system, Pretendard, 'Segoe UI', 'Roboto', 'Oxygen',
+      'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
+      sans-serif;
+   -webkit-font-smoothing: antialiased;
+   -moz-osx-font-smoothing: grayscale;
+   }
  
- `
+`
 export default GlobalStyle;

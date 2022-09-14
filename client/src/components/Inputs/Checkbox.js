938522bdc872c659d726ca0a509085a895d30c1f
jindory <laonjay@gmail.com>
2022-09-14 14:32:48 +0900

    chore. 회원가입 약관 전체 체크 (커스텀 전)

--- client/src/components/Inputs/Checkbox.js
+++ client/src/components/Inputs/Checkbox.js
@@ -1,26 +1,73 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import styled, { css } from "styled-components";
 
-const Checkbox = ({ text }) => {
-  const [isChecked, setIsChecked] = useState(false);
+// 약관 전체동의
+export const AllCheckbox = ({ text, onChangeHandler, checkItems, dataLength }) => {
 
   const onClickCheck = () => {
-    setIsChecked(!isChecked);
-    console.log(!isChecked);
+    
   };
 
   return (
     <CheckboxWrap>
-        <div onClick={onClickCheck} >
-            <Checkedbox type="checkbox" isChecked={isChecked}/>
-            <CheckedLabel isChecked={isChecked} ></CheckedLabel>
+        <div onClick={onClickCheck}>
+            <input 
+              type='checkbox' 
+              name='select-all'
+              checked={checkItems.length === dataLength ? true : false} 
+              onChange={(e) => onChangeHandler(e.target.checked)}
+            />
+            {/* <CheckedLabel isChecked={checkItems.length === dataLength ? true : false} ></CheckedLabel> */}
             <em>{text}</em>
         </div>
     </CheckboxWrap>
   );
 }
 
-export default Checkbox;
+// 약관 개별 동의
+export const EachCheckbox = ({ data, text, onChangeHandler, checkItems }) => {
+    const inputRef = useRef(null);
+
+    const onClickCheck = () => {
+
+    };
+
+    return (
+      <CheckboxWrap>
+          <div onClick={onClickCheck} >
+              <input 
+                type='checkbox' 
+                name={`select-${data.id}`}
+                checked={checkItems.includes(data.id) ? true : false} 
+                ref={inputRef}
+                onChange={(e) => onChangeHandler(e.target.checked, data.id)}
+              />
+              {/* <CheckedLabel isChecked={checkItems.includes(data.id) ? true : false} ></CheckedLabel> */}
+              <em>{text}</em>
+          </div>
+      </CheckboxWrap>
+    );
+  }
+
+
+export const Checkbox = ({ text, onChangeHandler }) => {
+    const [isChecked, setIsChecked] = useState(false);
+  
+    const onClickCheck = () => {
+      setIsChecked(!isChecked);
+    };
+  
+    return (
+      <CheckboxWrap>
+          <div onClick={onClickCheck} >
+              <Checkedbox type="checkbox" isChecked={isChecked} onChange={onChangeHandler}/>
+              <CheckedLabel isChecked={isChecked} ></CheckedLabel>
+              <em>{text}</em>
+          </div>
+      </CheckboxWrap>
+    );
+  }
+
 const CheckboxWrap = styled.div`
     position: relative;
     cursor: pointer;

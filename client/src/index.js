6f78932b1f73bc53bea4e420f4a5628c8b138430
Jin <laonjay@gmail.com>
2022-09-15 11:47:20 +0900

    chore. 견주 메인 - 강아지 정보  슬라이드 반영 (#9)
    * chore. slick 라이브러리 설치
    
    * chore. carousel 라이브러리 설치
    
    * chore. 견주 메인 - 강아지 정보 슬라이드 반영

--- client/src/index.js
+++ client/src/index.js
@@ -6,6 +6,7 @@ import GlobalStyle from "./GlobalStyle";
 import store from "./redux/store";
 import { Provider } from "react-redux";
 
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>

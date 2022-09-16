0c4cc04dae20661194d8366d792897fea22806bd
jindory <laonjay@gmail.com>
2022-09-16 23:02:10 +0900

    chore. 지난 산책 카드 컴포넌트 추가

--- client/src/pages/Owners/WalkerHistory.js
+++ client/src/pages/Owners/WalkerHistory.js
@@ -1,14 +1,17 @@
 import styled from "styled-components";
 import { Header } from "../../components/Layout/Header";
+import HistoryCard from "../../components/HistoryCard";
+
 
 const WalkerHistory = () =>{
     return(
         <div className="container bg-gray">
             <Header pageTitle={'춘식이 지난 산책 내역'} />
             <List>
-                <li>
-                    
-                </li>
+                <li> <HistoryCard /> </li>
+                <li> <HistoryCard /> </li>
+                <li> <HistoryCard /> </li>
+                <li> <HistoryCard /> </li>
             </List>
         </div>
     )
@@ -17,5 +20,8 @@ const WalkerHistory = () =>{
 export default WalkerHistory
 
 const List = styled.ul`
-
+    padding:5px 0 40px;
+    li+li{
+        margin-top:20px
+    }
 `

78bbcd3f7868147f9adda78e93d2d4f7129284b8
sohee kim <sohee063@nate.com>
2022-09-19 12:31:42 +0900

    Feat : 반려견 정보 수정 Feat : 반려견 삭제 Chore : 강아지 나이 표시 Chore : ownerMain 페이지 강아지 목록 띄우기

--- client/src/pages/Common/Setting/DogEdit.js
+++ client/src/pages/Common/Setting/DogEdit.js
@@ -1,127 +1,214 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import { HeaderConfirm } from "../../../components/Layout/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCamera } from '@fortawesome/free-solid-svg-icons';
-import { DogNameLabelType2, AnonymousLabelType2 } from "../../../components/DogNameLabel";
-import DatePicker from 'react-datepicker';
+import { faCamera } from "@fortawesome/free-solid-svg-icons";
+import {
+  DogNameLabelType2,
+  AnonymousLabelType2
+} from "../../../components/DogNameLabel";
+import DatePicker from "react-datepicker";
 import { DATE_FORMAT_CALENDAR } from "../../../assets/style/dateFormat";
 import { ko } from "date-fns/esm/locale";
-
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getMyPetInfo,
+  editMyPetInfo,
+  deleteMyPetInfo
+} from "../../../redux/actions/petActions";
+import { petSpecList } from "../../../constants/petSpecies";
+import { ButtonPrimary } from "../../../components/Button/Buttons";
 
 const DogEdit = () => {
-    const [startDate, setStartDate] = useState(new Date());
+  const dispatch = useDispatch();
+  const { myPetInfo } = useSelector((state) => state.pet);
 
-    const ClickHandler = () =>{
-        console.log('수정 확인 함수');
+  const [currentTab, setCurrentTab] = useState(0);
+  const [birth, setBirth] = useState();
+  const [myPetName, setMyPetName] = useState(myPetInfo[currentTab]?.petName);
+  const [myPetAbout, setMyPetAbout] = useState(myPetInfo[currentTab]?.petName);
+
+  const sex = useRef();
+  const spec = useRef();
+
+  let birthOfPet, year, month, day;
+  birthOfPet = myPetInfo[currentTab]?.birthday;
+  year = birthOfPet?.split("-")[0];
+  month = +birthOfPet?.split("-")[1];
+  day = +birthOfPet?.split("-")[2];
+
+  const ClickHandler = () => {
+    dispatch(
+      editMyPetInfo(
+        myPetInfo[currentTab].petId,
+        myPetName,
+        spec.current.value,
+        "2022-09-17T13:26:25.900Z",
+        sex.current.value,
+        myPetAbout
+      )
+    );
+  };
+
+  const deletePet = () => {
+    dispatch(deleteMyPetInfo(myPetInfo[currentTab].petId));
+  };
+
+  const menuArr = myPetInfo.map((el) => el);
+
+  const selectMenuHandler = (index) => {
+    setCurrentTab(index);
+  };
+
+  useEffect(() => {
+    dispatch(getMyPetInfo());
+  }, []);
+
+  useEffect(() => {
+    if (year && month && day) {
+      setBirth(new Date(Date.UTC(year, month - 1, day)));
     }
-    const menuArr = [
-        // 흠.. 탭 컨텐츠 객체인뎀.. 받아오는 데이터로 대체할 수 있을지..
-        { name: 'Tab1', content: 'Tab menu ONE' },
-        { name: 'Tab2', content: 'Tab menu TWO' },
-        { name: 'Tab3', content: 'Tab menu THREE' },
-    ];
+  }, [year, month, day]);
 
-    const [currentTab, setCurrentTab] = useState(0);
+  return (
+    <div className="container">
+      <HeaderConfirm
+        pageTitle={"강아지 정보 수정"}
+        ConfirmName={"완료"}
+        ClickHandler={ClickHandler}
+      />
+      <TabMenu>
+        {menuArr.map((el, index) => {
+          return (
+            <li
+              className={`${
+                index === currentTab ? "submenu focused" : "submenu"
+              }`}
+              onClick={() => selectMenuHandler(index)}
+              key={index}
+            >
+              <DogNameLabelType2 name={el.petName} key={index} />
+            </li>
+          );
+        })}
+        <li>
+          <Link to="/dogAdd">
+            <AnonymousLabelType2 />
+          </Link>
+        </li>
+      </TabMenu>
 
-    const selectMenuHandler = (index) => {
-        setCurrentTab(index)
-    };
+      <Desc>
+        {/* <p>{menuArr[currentTab].content}</p> */}
+        <UserInfo>
+          <div className="user-con">
+            <UserPhoto>
+              <img
+                src={"https://avatars.githubusercontent.com/u/9497404?v=4"}
+                className="user-photo"
+                alt=""
+              />
+              <Link to="/" className="user-edit">
+                <FontAwesomeIcon icon={faCamera} />
+              </Link>
+            </UserPhoto>
+          </div>
+        </UserInfo>
+        <Form>
+          <div className="ipt-group">
+            <label htmlFor="name" className="ipt-label">
+              강아지 이름
+            </label>
+            <input
+              type="text"
+              name="name"
+              className="ipt-form"
+              onChange={(e) => setMyPetName(e.target.value)}
+              defaultValue={myPetInfo[currentTab]?.petName}
+            />
+          </div>
+          <div className="ipt-group">
+            <label htmlFor="phone" className="ipt-label">
+              강아지 종
+            </label>
+            <select className="ipt-form" ref={spec}>
+              <option>{myPetInfo[currentTab]?.species}</option>
+              {petSpecList.map((el, idx) => (
+                <option key={idx}>{el}</option>
+              ))}
+            </select>
+          </div>
+          <div className="ipt-group">
+            <label htmlFor="" className="ipt-label">
+              강아지 생년월일
+            </label>
+            <DatePicker
+              locale={ko}
+              dateFormat="yyyy년 MM월 dd일 생"
+              selected={birth}
+              onChange={(date) => setBirth(date)}
+              maxDate={new Date()}
+              dateFormatCalendar={DATE_FORMAT_CALENDAR}
+            />
+          </div>
+          <div className="ipt-group">
+            <label htmlFor="" className="ipt-label">
+              강아지 성별
+            </label>
+            <select className="ipt-form" ref={sex}>
+              <option value={"M"}>수컷</option>
+              <option value={"F"}>암컷</option>
+            </select>
+          </div>
+          <div className="ipt-group">
+            <label htmlFor="" className="ipt-label">
+              소개글
+            </label>
+            <textarea
+              name=""
+              id=""
+              className="ipt-form"
+              onChange={(e) => setMyPetAbout(e.target.value)}
+              value={myPetInfo[currentTab]?.petName}
+            ></textarea>
+          </div>
 
-    return(
-        <div className="container">
-            <HeaderConfirm pageTitle={'강아지 정보 수정'} ConfirmName={'완료'} ClickHandler={ClickHandler}/>
-            <TabMenu>
-                {menuArr.map((li, index) => {
-                return <li  
-                className={`${index === currentTab ? 'submenu focused' : 'submenu'}`} 
-                onClick={()=>selectMenuHandler(index)}><DogNameLabelType2 name={'춘식'} /></li>
-                })}
-                <li><Link to="/dogAdd"><AnonymousLabelType2 /></Link></li>
-            </TabMenu>
-            
+          <ButtonPrimary onClick={deletePet}>등록 삭제하기</ButtonPrimary>
+        </Form>
+      </Desc>
+    </div>
+  );
+};
 
-            <Desc>
-                {/* <p>{menuArr[currentTab].content}</p> */}
-                <UserInfo>
-                    <div className="user-con">
-                        <UserPhoto>
-                            <img src={'https://avatars.githubusercontent.com/u/9497404?v=4'} className="user-photo" alt="" />
-                            <Link to="/" className="user-edit"><FontAwesomeIcon icon={faCamera}/></Link>
-                        </UserPhoto>
-                    </div>
-                </UserInfo>
-                <Form>
-                    <div className="ipt-group">
-                        <label htmlFor="name" className="ipt-label">강아지 이름</label>
-                        <input type="text" name="name" className="ipt-form" placeholder="강아지 이름을 입력해주세요." />
-                    </div>
-                    <div className="ipt-group">
-                        <label htmlFor="phone" className="ipt-label">강아지 종</label>
-                        <select className="ipt-form">
-                            <option>시바견</option>
-                            <option>시바견</option>
-                            <option>시바견</option>
-                        </select>
-                    </div>
-                    <div className="ipt-group">
-                        <label htmlFor="" className="ipt-label">강아지 생년월일</label>
-                        <DatePicker
-                            locale={ko}
-                            dateFormat="yyyy년 MM월 dd일 생"
-                            selected={startDate}
-                            onChange={(date) => setStartDate(date)}
-                            dateFormatCalendar={DATE_FORMAT_CALENDAR}
-                        />
-                    </div>
-                    <div className="ipt-group">
-                        <label htmlFor="" className="ipt-label">강아지 성별</label>
-                        <select className="ipt-form">
-                            <option>수컷</option>
-                            <option>암컷</option>
-                        </select>
-                    </div>
-                    <div className="ipt-group">
-                        <label htmlFor="" className="ipt-label">소개글</label>
-                        <textarea name="" id="" className="ipt-form" placeholder="강아지 상세 소개글을 입력해주세요."></textarea>
-                    </div>
-                </Form>
-            </Desc>
-            
-        </div>
-    )
-}
-
-export default DogEdit
+export default DogEdit;
 const Form = styled.div`
-    padding-bottom:60px;
-`
+  padding-bottom: 60px;
+`;
 
 const UserInfo = styled.section`
-    text-align: center;
-    padding:10px 0 3px;
-`
-
+  text-align: center;
+  padding: 10px 0 3px;
+`;
 
 const TabMenu = styled.ul`
   font-weight: bold;
   overflow: auto;
   white-space: nowrap;
-  margin:0 -20px;
-  padding:10px 15px 12px;
-  border-bottom:1px solid var(--gray-200);
-  
-  li{
-    position:relative;
+  margin: 0 -20px;
+  padding: 10px 15px 12px;
+  border-bottom: 1px solid var(--gray-200);
+
+  li {
+    position: relative;
     display: inline-block;
-    margin-right:9px;
+    margin-right: 9px;
   }
 
   .focused {
-    span{
-        background:var(--primary);
-        color:var(--white-000)
+    span {
+      background: var(--primary);
+      color: var(--white-000);
     }
   }
 
@@ -131,39 +218,38 @@ const TabMenu = styled.ul`
 `;
 
 const Desc = styled.div`
-    border-top: 7px solid var(--gray-100);
-    margin:0 -20px;
-    padding:20px;
+  border-top: 7px solid var(--gray-100);
+  margin: 0 -20px;
+  padding: 20px;
 `;
 
-
 const UserPhoto = styled.div`
-    display: inline-block;
-    position:relative;
-    margin-bottom:13px;
+  display: inline-block;
+  position: relative;
+  margin-bottom: 13px;
 
-    .user-photo{
-        width:100px;
-        height:100px;
-        border-radius: 100px;
-    }
-    
-    .user-edit{
-        position:absolute;
-        right:-7px;
-        bottom:-3px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width:35px;
-        height:35px;
-        background:var(--gray-100);
-        border:3px solid var(--white-000);
-        box-shadow: 0 0 15px 0 rgba(0,0,0, .1);
-        border-radius: 50%;
+  .user-photo {
+    width: 100px;
+    height: 100px;
+    border-radius: 100px;
+  }
 
-        svg{
-            color:var(--gray-800)
-        }
+  .user-edit {
+    position: absolute;
+    right: -7px;
+    bottom: -3px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 35px;
+    height: 35px;
+    background: var(--gray-100);
+    border: 3px solid var(--white-000);
+    box-shadow: 0 0 15px 0 rgba(0, 0, 0, 0.1);
+    border-radius: 50%;
+
+    svg {
+      color: var(--gray-800);
     }
-`
+  }
+`;
--- client/src/pages/Owners/OwnerMain.js
+++ client/src/pages/Owners/OwnerMain.js
@@ -1,139 +1,161 @@
-import styled from "styled-components"
+import styled from "styled-components";
 import Slider from "react-slick";
-import { ReactComponent as Logo } from '../../assets/img/logo.svg';
+import { ReactComponent as Logo } from "../../assets/img/logo.svg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBell } from '@fortawesome/free-solid-svg-icons';
-import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
-import { NextArrow, PrevArrow } from "../../components/Button/SliderArrowButton";
+import { faBell } from "@fortawesome/free-solid-svg-icons";
+import { faLocationDot } from "@fortawesome/free-solid-svg-icons";
+import {
+  NextArrow,
+  PrevArrow
+} from "../../components/Button/SliderArrowButton";
 import { DogCard, AnonymousDogCard } from "../../components/DogCard";
 import PlaceList from "../../components/PlaceList";
 import Nav from "../../components/Layout/Nav";
+import { useSelector, useDispatch } from "react-redux";
+import { getMyPetInfo } from "../../redux/actions/petActions";
+import { useEffect } from "react";
 
 const OwnerMain = () => {
-    const settings = {
-        slide: 'div',
-        dots: true,  
-        arrows : true,
-        infinite: false, 
-        speed: 1000,
-        autoplay:false,
-        slidesToShow: 1, 
-        slidesToScroll: 1, 
-        dotsClass : "slick-dots",
-        draggable : true,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-    };
-    
-    return(
-        <div className="container bg-gray">
-            <Header>
-                <LogoArea>
-                    <Logo/>
-                </LogoArea>
-                <Alert>
-                    <FontAwesomeIcon icon={faBell}/>
-                </Alert>
-            </Header>
-            <Section>
-                <DogSlide>
-                    <Slider {...settings}>
-                        <DogCard />
-                        <AnonymousDogCard/>
-                    </Slider>
-                </DogSlide>
-            </Section>
-            <Section>
-                <Weather>
-                    <p>날씨API 대기쓰</p>
-                </Weather>
-            </Section>
-            <InfoSection>
-                <h3>주변 강아지 동반 카페 <small><FontAwesomeIcon icon={faLocationDot}/> 강남구</small></h3>
-                <PlaceList />
-            </InfoSection>
+  const dispatch = useDispatch();
+  const { myPetInfo } = useSelector((state) => state.pet);
 
-            <Nav />
-        </div>
-    )
-}
+  const settings = {
+    slide: "div",
+    dots: true,
+    arrows: true,
+    infinite: true,
+    speed: 1000,
+    autoplay: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    dotsClass: "slick-dots",
+    draggable: true,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />
+  };
 
-export default OwnerMain
+  useEffect(() => {
+    dispatch(getMyPetInfo());
+  }, []);
 
+  return (
+    <div className="container bg-gray">
+      <Header>
+        <LogoArea>
+          <Logo />
+        </LogoArea>
+        <Alert>
+          <FontAwesomeIcon icon={faBell} />
+        </Alert>
+      </Header>
+      <Section>
+        <DogSlide>
+          <Slider {...settings}>
+            {myPetInfo.map((el, idx) => (
+              <DogCard key={idx} props={el} />
+            ))}
+
+            <AnonymousDogCard />
+          </Slider>
+        </DogSlide>
+      </Section>
+      <Section>
+        <Weather>
+          <p>날씨API 대기쓰</p>
+        </Weather>
+      </Section>
+      <InfoSection>
+        <h3>
+          주변 강아지 동반 카페{" "}
+          <small>
+            <FontAwesomeIcon icon={faLocationDot} /> 강남구
+          </small>
+        </h3>
+        <PlaceList />
+      </InfoSection>
+
+      <Nav />
+    </div>
+  );
+};
+
+export default OwnerMain;
 
 const Header = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding:15px;
-`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+`;
 const LogoArea = styled.div`
-    svg{width:55px};
-`
+  svg {
+    width: 55px;
+  }
+`;
 
 const Alert = styled.div`
-    font-size:19px;
-    color:var(--gray-600);
-`
+  font-size: 19px;
+  color: var(--gray-600);
+`;
 
 const Section = styled.section`
-    background:var(--white-000);
-    padding:20px 0;
-    border-radius:25px;
-    margin-bottom:10px;
-`
+  background: var(--white-000);
+  padding: 20px 0;
+  border-radius: 25px;
+  margin-bottom: 10px;
+`;
 
 const DogSlide = styled.div`
-    padding-bottom:20px;
-    .slick-slide{
-        padding:0 20px;
+  padding-bottom: 20px;
+  .slick-slide {
+    padding: 0 20px;
+  }
+
+  .slick-dots {
+    li {
+      width: auto;
+      margin: 0 4px;
+
+      button {
+        width: 5px;
+        height: 5px;
+        background-color: var(--gray-300);
+        border-radius: 50px;
+        transition: all 0.5s;
+      }
+      button:before {
+        content: "";
+      }
     }
 
-    .slick-dots{
-       li{ 
-        width: auto;
-        margin:0 4px;
-
-        button{
-            width:5px;
-            height:5px;
-            background-color:var(--gray-300);
-            border-radius: 50px;
-            transition: all .5s;
-        }
-        button:before{
-            content: '';
-        }
-       }
-
-       .slick-active{
-            button{
-                width:22px;
-                background-color: var(--primary);
-            }
-       }
+    .slick-active {
+      button {
+        width: 22px;
+        background-color: var(--primary);
+      }
     }
-`
+  }
+`;
 
 const Weather = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    height:60px;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60px;
+`;
 
 const InfoSection = styled.section`
-    margin:20px 0 100px;
-    h3{
-        font-size:17px;
-        small{
-            font-size:13px;
-            margin-left:9px;
-            
-            svg{
-                color:var(--gray-400);
-                font-size:12px;
-            }
-        }
+  margin: 20px 0 100px;
+  h3 {
+    font-size: 17px;
+    small {
+      font-size: 13px;
+      margin-left: 9px;
+
+      svg {
+        color: var(--gray-400);
+        font-size: 12px;
+      }
     }
-`
+  }
+`;

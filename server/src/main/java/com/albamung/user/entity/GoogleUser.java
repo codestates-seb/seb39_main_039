dd962403cf2c9caa6d16c1d297f241e789d6fbc9
Nabi-Yi <nabiyi926@gmail.com>
2022-09-19 18:22:37 +0900

    chore : 개발중 코드

---
+++ server/src/main/java/com/albamung/user/entity/GoogleUser.java
@@ -0,0 +1,16 @@
+//package com.albamung.user.entity;
+//
+//import java.util.Map;
+//
+//public class GoogleUser extends User{
+//    public GoogleUser(Map<String,Object> attributes){
+//        this.email = attributes.get("email").toString();
+//        this.nickName = attributes.get("given_name").toString();
+//        this.fullName = attributes.get("name").toString();
+//        this.provider = "GOOGLE";
+//        this.providerId = attributes.get("sub").toString();
+//        this.password = "12345678";
+//        this.roles = "ROLE_USER";
+//        this.profileImage = attributes.get("picture").toString();
+//    }
+//}

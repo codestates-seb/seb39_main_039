25b0e79d5e84a64c6e1909a8c1c351596ae9f60d
jindory <laonjay@gmail.com>
2022-09-25 21:04:24 +0900

    feat. 뒤로가기 useNavigate 추가

--- client/src/components/Layout/Header.js
+++ client/src/components/Layout/Header.js
@@ -3,14 +3,16 @@ import styled from "styled-components";
 import { ReactComponent as IconBack } from '../../assets/img/hdIcon-back.svg';
 import { ReactComponent as IconSearch } from '../../assets/img/hdIcon-search.svg';
 import { ReactComponent as Close } from '../../assets/img/close.svg';
-
+import { useNavigate } from "react-router-dom";
 // 우측 아이콘도 사용할 경우 useRight 'on'
 // <Header pageTitle={'로그인'} useRight="on"></Header>
+
 export const Header = ({pageTitle, useRight}) => {
+    const navigate = useNavigate()
     return(
         <Conpanel>
             <div className="left-area">
-                <Link to ="/"><IconBack /></Link>
+                <button className="btn-back" onClick={() => navigate(-1)}><IconBack /></button>
             </div>
             <h2>{pageTitle}</h2>
         <div className={`right-area ${useRight}`}>
@@ -21,24 +23,26 @@ export const Header = ({pageTitle, useRight}) => {
 }
 
 export const HeaderClose = ({pageTitle}) => {
+    const navigate = useNavigate()
     return(
         <Conpanel>
             <div className="left-area">
-                <Link to ="/"></Link>
+                <button className="btn-back" onClick={() => navigate(-1)}></button>
             </div>
             <h2>{pageTitle}</h2>
             <div className="right-area on">
-                <Link to ="/"><Close/></Link>
+                <button className="btn-back" onClick={() => navigate(-1)}><Close/></button>
             </div>
         </Conpanel>
     )
 }
 
 export const HeaderConfirm = ({pageTitle, useRight, ConfirmName, ClickHandler}) => {
+    const navigate = useNavigate()
     return(
         <Conpanel>
             <div className="left-area">
-                <Link to ="/"><IconBack /></Link>
+                <button className="btn-back" onClick={() => navigate(-1)}><IconBack /></button>
             </div>
             <h2 className="confirm">{pageTitle}</h2>
             <div className={`right-area on confirm`} onClick={ClickHandler}>
@@ -59,7 +63,12 @@ const Conpanel = styled.div`
     font-size:18px;
     font-weight: 500;
     z-index: 99;
-
+   
+    .btn-back{
+        border:0;
+        background:0;
+    }
+    
     h2{
         flex:5;
         text-align: center;
@@ -78,13 +87,13 @@ const Conpanel = styled.div`
     .right-area{
         text-align: right;
         margin-right: -10px;
-        a {
+        button {
             display: none;
         }
     }
 
     .right-area.on{
-        a {
+        button {
             display: block;
         }
     }
@@ -96,4 +105,5 @@ const Conpanel = styled.div`
         color:var(--primary);
         font-weight: 600;
     }
+    
 `

7941feac6660a5f1096c80d941625dea08777c82
jindory <laonjay@gmail.com>
2022-09-27 16:04:50 +0900

    chore. 지난 산책내역 헤더 강아지 이름 출력

--- client/src/pages/Owners/WalkerHistory.js
+++ client/src/pages/Owners/WalkerHistory.js
@@ -3,6 +3,7 @@ import Lottie from "lottie-react";
 import { Header } from "../../components/Layout/Header";
 import HistoryCard from "../../components/HistoryCard";
 import { getPetWalkInfo } from "../../redux/actions/petwalkActions";
+import { getMyPetInfo } from "../../redux/actions/petActions";
 import { useParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect } from "react";
@@ -12,14 +13,16 @@ const WalkerHistory = () => {
   const petId = useParams();
   const dispatch = useDispatch();
   const petWalkInfo = useSelector((state) => state.petwalk.petWalkInfo);
-
+  const myPetInfo = useSelector((state)=>state.pet.myPetInfo);
   useEffect(() => {
     dispatch(getPetWalkInfo(Number(petId.id)));
+    dispatch(getMyPetInfo())
   }, []);
-  
+  const petName = myPetInfo.filter((el)=> el.petId === Number(petId.id))
+
   return (
     <div className="container bg-gray">
-      <Header pageTitle={`춘식이 지난 산책 내역`} />
+      <Header pageTitle={`${petName[0].petName} 지난 산책 내역`} />
       {petWalkInfo.items?.length !== 0 ? (
         <List>
           {petWalkInfo.items?.map((el, idx) => {

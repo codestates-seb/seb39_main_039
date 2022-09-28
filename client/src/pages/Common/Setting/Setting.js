bcae1ee3712d2739f05078ee5134e680fe7d4dd6
sohee kim <sohee063@nate.com>
2022-09-28 22:19:11 +0900

    Chore : walkerMain, walkerCard 충돌 코드 수정(currentWalk 없을 때 에러 처리 등…) Chore : 전체적인 Time 존 수정 중 Feat : 구인글 작성자 기준 수정/삭제 조건부 출력 Chore : 강아지 정보 수정시 tap번호 로컬스토리지 저장, 그 밖의 수상한 에러들 해겨얼… Feat : 견주/알바모드에 따라 홈버튼 눌렀을 때 견주/알바 메인페이지로 다르게 이동

--- client/src/pages/Common/Setting/Setting.js
+++ client/src/pages/Common/Setting/Setting.js
@@ -15,7 +15,6 @@ import { getUserInfo } from "../../../redux/actions/userActions";
 import { useNavigate } from "react-router-dom";
 import { getMyPetInfo } from "../../../redux/actions/petActions";
 import Modal from "../../../components/Modal/Modal";
-import { parseJSON } from "date-fns";
 
 const Setting = () => {
   const dispatch = useDispatch();

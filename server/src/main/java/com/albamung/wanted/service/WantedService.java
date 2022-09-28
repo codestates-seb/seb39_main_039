5a1bca69848054e3657619c1174cc6d5ad1ed428
Nabi-Yi <nabiyi926@gmail.com>
2022-09-29 01:37:52 +0900

    fix: 산책 미사용 필드변수 제거

--- server/src/main/java/com/albamung/wanted/service/WantedService.java
+++ server/src/main/java/com/albamung/wanted/service/WantedService.java
@@ -59,7 +59,6 @@ public class WantedService {
                 .startTime(request.getStartTime())
                 .endTime(request.getEndTime())
                 .caution(request.getCaution())
-                .coord("0")
                 .build();
         walk.setCheckListByContents(request.getCheckListContent());
 
